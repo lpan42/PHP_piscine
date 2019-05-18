35dff5af9b82702d8983cd1c2e8742f815e7f0da
Le PAN <lpan@e1r13p11.42.fr>
2019-05-18 16:56:49 +0200

    adding logout page

--- D04/ex04/logout.php
+++ D04/ex04/logout.php
@@ -3,3 +3,16 @@
     session_unset(); //destroy a single session variable
     // session_destroy();destroy all the session variables
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+<body>
+    <p>You have successfully logged out.</p>
+    <a href="index.html">Return</a>
+</body>
+</html>
