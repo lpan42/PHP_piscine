c54a9171624f5e9b1fc368e3bd3c7972da11d03d
Le PAN <lpan@e1r9p6.42.fr>
2019-05-23 16:25:26 +0200

    finish d02

--- D02/ex02/magnifying_glass.php
+++ D02/ex02/magnifying_glass.php
@@ -2,13 +2,13 @@
 <?php
 
     $file = file_get_contents($argv[1]);
-    
+    $file = str_replace("\n", "AAA", $file); 
     function titleupper($matches)
     {
         //printf("match0:%s\n", $matches[0]);
         return ($matches[1].strtoupper($matches[2]).$matches[3]);
     }
-    
+   
     function linkname_upper($matches)
     {
         return ($matches[1].strtoupper($matches[2]).$matches[3]);
@@ -18,14 +18,18 @@
     {
         //print_r($matches);
         //printf("begin:%s\n", $matches[0]);
-        $matches[0]= preg_replace_callback("/( title=\")(.*?)(\")/", "titleupper", $matches[0]);
-       // printf("title:%s\n", $matches[0]);
+       $matches[0]= preg_replace_callback("/( title=\")(.*?)(\")/", "titleupper", $matches[0]);
+        //printf("title:%s\n", $matches[0]);
+       
+       $matches[0] = preg_replace_callback("/(>)(.*?)(<)/", "linkname_upper", $matches[0]);
+       //printf("linkname:%s\n", $matches[0]);
 
         $matches[0] = preg_replace_callback("/(>)(.*?)(<)/", "linkname_upper", $matches[0]);
         //printf("linkname:%s\n", $matches[0]);
         return ($matches[0]);
     }
-    $res = preg_replace_callback("/(<a )(.|\n)*(<\/a>)/", "wholelink", $file);
+    $res = preg_replace_callback("/(<a )(.*?)(<\/a>)/", "wholelink", $file);
+    $res = str_replace("AAA", "\n", $res); 
     echo $res;
     //$matches[0] is the complete match
     //$matches[1,2,3] the match for the first subpattern
