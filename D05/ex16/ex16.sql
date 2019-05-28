cc8436435c1406ee8e7abd5f1cd1877c0c083124
Le PAN <lpan@e3r7p2.42.fr>
2019-05-28 21:52:00 +0200

    change d05ex16

--- D05/ex16/ex16.sql
+++ D05/ex16/ex16.sql
@@ -1,3 +1,9 @@
-SELECT COUNT(`member_history`.`id_film`) AS 'movies' FROM `member_history`
-    WHERE (`member_history`.`date` BETWEEN DATE('2006-10-30') AND DATE('2007-07-27'))
-    OR DATE_FORMAT(`member_history`.`date`, '%m-%d') = '12-24';
+SELECT COUNT(member_history.`id_film`) AS `movies` FROM member_history
+    WHERE (member_history.`date` BETWEEN DATE('2006-10-30') AND DATE('2007-07-27'))
+    OR DATE_FORMAT(member_history.`date`, '%m-%d') = '12-24';
+
+
+
+    -- SELECT COUNT(`id_film`) AS `movies` FROM member_history
+    -- WHERE `date` BETWEEN '2006-10-30' AND '2007-07-27'
+    -- OR MONTH(`date`) = '12' AND DAY(`date`) = '24';
