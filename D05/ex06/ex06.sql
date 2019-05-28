a49c3de69a741408c7bd4c2dc9caea58db32575d
Le PAN <lpan@e3r7p2.42.fr>
2019-05-28 22:50:33 +0200

    WTF D06

--- D05/ex06/ex06.sql
+++ D05/ex06/ex06.sql
@@ -1,3 +1,3 @@
 SELECT `title`, `summary` FROM film
-    WHERE `summary` LIKE "%Vincent%"
-    ORDER BY `id_film` ASC;
+   WHERE `summary` LIKE LOWER('%Vincent%')
+   ORDER BY `id_film` ASC;
--- D06/ex02/Vector.class.php
+++ D06/ex02/Vector.class.php
@@ -1,4 +1,5 @@
 <?php
+require_once('Vertex.class.php');
 class Vector{
     private $_x;
     private $_y;
@@ -7,18 +8,17 @@ class Vector{
     static $verbose = FALSE;
 
     function __construct($arr){
-        if(isset($arr['x']) && isset($arr['y']) && isset($arr['z'])){
-            $this->_x = $arr['x'];
-            $this->_y = $arr['y'];
-            $this->_z = $arr['z'];
-            if(isset($arr['w'])){
-                $this->_w = $arr['w'];
-            }
-            if(isset($arr['color'])){
-                $this->_color = $arr['color'];
+        if(isset($arr['dest']) && $arr['dest'] instanceof Vertex){
+            if(isset($arr['orig']) && $arr['orig'] instanceof Vertex){
+                $orig = new Vertex(array('x' => $arr['orig']->get_x(), 
+                                        'y' => $arr['orig']->get_y(),
+                                        'z' => $arr['orig']->get_z()));
             }else{
-                $this->_color = new Color(array('red' => 255, 'green'=> 255, 'blue'=> 255));
+                $orig = new Vertex(array('x' => 0, 'y' => 0, 'z' => 0, 'w'=> 1));
             }
+            $this->_x = $arr['dest']->get_x() - $orig->get_x();
+            $this->_y = $arr['dest']->get_y() - $orig->get_y();
+            $this->_z = $arr['dest']->get_z() - $orig->get_z();
             if(Self::$verbose == TRUE){
                 echo $this->__toString() . " constructed.\n";
             }
@@ -27,12 +27,8 @@ class Vector{
 
     function __toString(){
         if(Self::$verbose == TRUE) {
-            return (sprintf("Vertex( x: %0.2f, y: %0.2f, z: %0.2f, w: %0.2f, Color( red: %3d, green: %3d, blue: %3d ) )", 
-                $this->_x, $this->_y, $this->_z, $this->_w, 
-                $this->_color->red, $this->_color->red, $this->_color->red));
-        }else{
-            return (sprintf("Vertex( x: %0.2f, y: %0.2f, z: %0.2f, w: %0.2f )", 
-                $this->_x, $this->_y, $this->_z, $this->_w));
+            return (sprintf("Vector( x:%0.2f, y:%0.2f, z:%0.2f, w:%0.2f ))", 
+                    $this->_x, $this->_y, $this->_z, $this->_w));
         }
     }
 
@@ -43,7 +39,36 @@ class Vector{
     }
         
     static function doc() {
-        echo file_get_contents("Vertex.doc.txt");
+        echo file_get_contents("Vector.doc.txt");
     }
+
+    public function get_x(){
+        return $this->_x;
+    }
+
+    public function get_y(){
+        return $this->_y;
+    }
+
+    public function get_z(){
+        return $this->_z;
+    }
+
+    public function magnitude(){
+        return sqrt(pow($this->_x, 2) + pow($this->_y, 2) + pow($this->_z, 2));
+    }
+
+    public function normalize(){
+        $length = $this->magnitude();
+        if ($length === 1){
+            return (new vector(array('orig'=> $arr['orig'], 'dest' => $arr['dest'])));
+        }else{
+            return (new vector(array('dest' => new Vertex(array('x'=>$this->_x / $length,
+                                                                'y'=>$this->_y / $length,
+                                                                'z'=>$this->_z / $length)))));
+        }
+    }
+
+    
 }
 ?>
